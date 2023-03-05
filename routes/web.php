72aa52a4e8716a64d36c9929b495d9fe2b793e46
Fabrice Gagneux <fabrice@goodbodywellness.co.uk>
2023-03-05 17:12:19 +0000

    bracket

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,11 @@ $router->get('/now_showing', function () {
 $router->get('/coming_soon', function () {
 	return redirect()->route('wanted4k');
 });
-$router->get('/4k]', ['as' => '4k', 'uses' => 'MoviesController@MyMovies4k']);
-$router->get('/blu-rays]', ['as' => 'br', 'uses' => 'MoviesController@MyMoviesBR']);
-$router->get('/steelbooks]', ['as' => 'steel', 'uses' => 'MoviesController@MyMoviesSteel']);
-$router->get('/wanted-4k]', ['as' => 'wanted4k', 'uses' => 'MoviesController@Wanted4k']);
-$router->get('/wanted-blu-rays]', ['as' => 'wantedbr', 'uses' => 'MoviesController@WantedBR']);
+$router->get('/4k', ['as' => '4k', 'uses' => 'MoviesController@MyMovies4k']);
+$router->get('/blu-rays', ['as' => 'br', 'uses' => 'MoviesController@MyMoviesBR']);
+$router->get('/steelbooks', ['as' => 'steel', 'uses' => 'MoviesController@MyMoviesSteel']);
+$router->get('/wanted-4k', ['as' => 'wanted4k', 'uses' => 'MoviesController@Wanted4k']);
+$router->get('/wanted-blu-rays', ['as' => 'wantedbr', 'uses' => 'MoviesController@WantedBR']);
 
 $router->get('/movie_detail/{movie_id}', ['as' => 'movieDetail', 'uses' => 'MoviesController@MovieDetail']);
 //$router->get('/genres', 'MoviesController@Genres');
