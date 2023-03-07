d4d998e683e5c63b48a09a4ac90814d6e5749f02
Fabrice Gagneux <fabrice@goodbodywellness.co.uk>
2023-03-07 07:03:35 +0000

    start solving

--- app/Console/Commands/SudokuSolver.php
+++ app/Console/Commands/SudokuSolver.php
@@ -40,7 +40,26 @@ class SudokuSolver extends Command
             ];
         }
         
+        $sol = [ 
+            56 => 5, 
+        ];
+        foreach ($sol as $key => $value) {
+            $binValue = decbin($value);
+            $col = ceil($key / 9);
+            $row = $key % 9 === 0 ? 9 : $key % 9;
+            $cel = (floor(($col - 1) / 3) * 3 )+ ceil($row / 3);
+            print ($binValue);
+
+            foreach($grid as $k => $c) {
+                if ($c['column'] == $col || $c['row'] == $row || $c['cell'] == $cel ) {
+                    $grid[$k]['values'] = $c['values'] - $binValue;
+                }
+            }
+        } 
+
         print_r($grid);
+
+
     }
 
     
