3924890e2f542eed0305a5e3b51840e02a6be80a
Fabrice Gagneux <fabrice@goodbodywellness.co.uk>
2023-03-07 06:14:58 +0000

    sudoku solver

---
+++ app/Console/Commands/SudokuSolver.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+
+class SudokuSolver extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sudoku:solve';
+ 
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'solve a sudoku testing';
+ 
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        echo "hello world";
+        
+    }
+
+    
+}
