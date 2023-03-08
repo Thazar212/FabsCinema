652bf4d7caccb30730e739fa38e250b0b0517277
Fabrice Gagneux <fabrice@goodbodywellness.co.uk>
2023-03-08 10:55:14 +0000

    debug code

--- app/Console/Commands/SudokuSolver.php
+++ app/Console/Commands/SudokuSolver.php
@@ -385,8 +385,8 @@ class SudokuSolver extends Command
                                     print ("{$cellIndex} : {$allTrue}\n");
                                     for ($n = 0; $n < strlen($cellValue); $n++) {
                                         if ($this->isKthBitSet($cellValue, $n)) {
-                                            if ((in_array($n +1, $indexes) && !$allTrue) || (!in_array($n +1, $indexes) && $allTrue)) {
-                                                print "remove Index {$index} from cell {$cellIndex} \n";
+                                            if ((in_array($n + 1, $indexes) && !$allTrue) || (!in_array($n + 1, $indexes) && $allTrue)) {
+                                                print "remove Index {$n + 1} from cell {$cellIndex} \n";
                                             }
         
                                         }
