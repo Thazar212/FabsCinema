3b1957f82a376a6f1e8d969c4f6e0d079630b901
Fabrice Gagneux <fabrice@goodbodywellness.co.uk>
2023-03-08 10:51:53 +0000

    remove exact equality

--- app/Console/Commands/SudokuSolver.php
+++ app/Console/Commands/SudokuSolver.php
@@ -385,7 +385,7 @@ class SudokuSolver extends Command
                                     print ("{$cellIndex} : {$allTrue}\n");
                                     for ($n = 0; $n < strlen($cellValue); $n++) {
                                         if ($this->isKthBitSet($cellValue, $n)) {
-                                            if (($n === $index -1 && !$allTrue) || ($n !== $index -1 && $allTrue)) {
+                                            if (($n == $index -1 && !$allTrue) || ($n != $index -1 && $allTrue)) {
                                                 print "remove Index {$index} from cell {$cellIndex} \n";
                                             }
         
